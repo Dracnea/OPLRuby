f50f5421bd88b1c8b420ce4e54f343e0492d57d6
Nick Ashenfelter <nashenfelter@gmail.com>
2019-11-18 18:13:05 -0500

    Set Up Title Text

--- main.rb
+++ main.rb
@@ -1,13 +1,15 @@
-class MyClass
-  def say_hello
-    puts "Hello World"
-  end
+def titleScreen
+  puts "__Game Title__"
+  puts "\n\n"
+  puts "Created By Nick A, Sam W, Tyler R, and Stefan"
+  puts "Version 1.0.0"
+  puts "\n\n"
+  puts "Press Any Key To Continue..."
 end
+
 
 def another_hello
   puts "Hello World (from a method)"
 end
 
-c = MyClass.new
-c.say_hello
-another_hello
+titleScreen
